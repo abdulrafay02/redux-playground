9c9ec08910f65dca812913e18334b2fb273608cd
abdulrafay_02 <abdulrafay_02@outlook.com>
2018-05-07 22:16:57 +0500

    working on post reducer

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from "react-redux"
+
+import Posts from "./components/posts"
+import PostForm from "./components/postForm"
+import store from "./store"
+
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Redux Example</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-content">
+          <PostForm/>
+          <hr/>
+          <Posts/>
+        </div>
       </div>
+      </Provider>
     );
   }
 }
